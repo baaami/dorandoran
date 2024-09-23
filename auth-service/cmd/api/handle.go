478292440ac400ea02f1abf0e0df9ae2833ca139
baaami <lgh121546@naver.com>
2024-09-23 16:25:34 +0900

    snsID를 Unique하게 사용할 수 있도록 수정
    [변경된 모듈]
    auth-service
    
    [세부 사항]
    redis에 session id와 함께 snsID를 저장하도록 수정
    
    snsID: ${SNS_TYPE}:${SNS_ID}

--- auth-service/cmd/api/handle.go
+++ auth-service/cmd/api/handle.go
@@ -6,9 +6,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"time"
 
-	"github.com/google/uuid"
+	"github.com/baaami/dorandoran/auth/pkg/types"
 )
 
 // Redis 클라이언트 생성
@@ -47,12 +46,13 @@ func (app *Config) KakaoLoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 사용자 정보에서 카카오 사용자 ID 추출
 	kakaoUserID := fmt.Sprintf("%v", kakaoResponse["id"])
+	snsID := app.RedisClient.MakeSnsID(types.KAKAO, kakaoUserID)
 
 	// 기존 세션이 존재하는지 확인
-	sessionID, err := app.RedisClient.GetSessionByUserID(kakaoUserID)
+	sessionID, err := app.RedisClient.GetSessionBySnsID(snsID)
 	if err != nil {
 		// 세션이 존재하지 않으면 새로 생성
-		sessionID = app.CreateSession(kakaoUserID)
+		sessionID = app.RedisClient.CreateSession(snsID)
 	}
 
 	cookie := &http.Cookie{
@@ -67,22 +67,4 @@ func (app *Config) KakaoLoginHandler(w http.ResponseWriter, r *http.Request) {
 	// 클라이언트에게 로그인 성공 응답
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Login successful, session ID issued"))
-}
-
-// 세션 생성 함수 (Redis에 세션 저장)
-func (app *Config) CreateSession(kakaoUserID string) string {
-	// 고유한 세션 ID 생성 (UUID 사용)
-	sessionID := uuid.New().String()
-
-	// 세션 만료 시간 설정 (예: 24시간)
-	expiresAt := time.Hour * 24
-
-	// Redis에 세션 저장
-	err := app.RedisClient.SetSession(sessionID, kakaoUserID, expiresAt)
-	if err != nil {
-		fmt.Printf("Failed to store session in Redis: %v", err)
-	}
-
-	// 생성된 세션 ID 반환
-	return sessionID
 }
--- auth-service/pkg/redis/redis.go
+++ auth-service/pkg/redis/redis.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/baaami/dorandoran/auth/pkg/types"
 	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 )
 
 var ctx = context.Background()
@@ -42,8 +45,8 @@ func NewRedisClient() (*RedisClient, error) {
 }
 
 // SetSession: Redis에 세션 저장
-func (r *RedisClient) SetSession(sessionID string, userID string, expiresAt time.Duration) error {
-	err := r.Client.Set(ctx, sessionID, userID, expiresAt).Err()
+func (r *RedisClient) SetSession(sessionID string, snsID string, expiresAt time.Duration) error {
+	err := r.Client.Set(ctx, sessionID, snsID, expiresAt).Err()
 	if err != nil {
 		log.Printf("Failed to set session in Redis: %v", err)
 		return err
@@ -53,13 +56,13 @@ func (r *RedisClient) SetSession(sessionID string, userID string, expiresAt time
 
 // GetSession: Redis에서 세션 조회
 func (r *RedisClient) GetSession(sessionID string) (string, error) {
-	userID, err := r.Client.Get(ctx, sessionID).Result()
+	snsID, err := r.Client.Get(ctx, sessionID).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("session not found")
 	} else if err != nil {
 		return "", err
 	}
-	return userID, nil
+	return snsID, nil
 }
 
 // DeleteSession: Redis에서 세션 삭제
@@ -72,13 +75,39 @@ func (r *RedisClient) DeleteSession(sessionID string) error {
 	return nil
 }
 
-// GetSessionByUserID: 사용자 ID로 세션 ID 조회
-func (r *RedisClient) GetSessionByUserID(userID string) (string, error) {
-	sessionID, err := r.Client.Get(ctx, userID).Result()
+// GetSessionBySnsID: 사용자 ID로 세션 ID 조회
+func (r *RedisClient) GetSessionBySnsID(snsID string) (string, error) {
+	sessionID, err := r.Client.Get(ctx, snsID).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("no session found for user")
 	} else if err != nil {
 		return "", err
 	}
 	return sessionID, nil
+}
+
+// GetSessionBySnsID: 사용자 ID로 세션 ID 조회
+func (r *RedisClient) MakeSnsID(snsType int, snsUserID string) string {
+	snsTypeStr := strconv.Itoa(snsType)
+	snsID := snsTypeStr + types.USER_ID_DELIMETER + snsUserID
+
+	return snsID
+}
+
+// 세션 생성 함수 (Redis에 세션 저장)
+func (r *RedisClient) CreateSession(snsID string) string {
+	// 고유한 세션 ID 생성 (UUID 사용)
+	sessionID := uuid.New().String()
+
+	// 세션 만료 시간 설정 (예: 24시간)
+	expiresAt := time.Hour * 24
+
+	// Redis에 세션 저장
+	err := r.SetSession(sessionID, snsID, expiresAt)
+	if err != nil {
+		fmt.Printf("Failed to store session in Redis: %v", err)
+	}
+
+	// 생성된 세션 ID 반환
+	return sessionID
 }
