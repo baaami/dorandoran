4f3d6064087678fcf51055fb3f5878ca5b9374dd
baaami <lgh121546@naver.com>
2024-09-24 12:54:52 +0900

    auth-service 내 카카오 로그인/회원가입 기능 개발
    [변경된 모듈]
    auth-service
    
    [세부 사항]
    
    유저 존재 여부를 Kakao id를 사용하여 회원 DB에서 확인
    
    1. 유저가 존재하지 않는 경우
    
    (1) 회원 DB에 회원 등록
    (2) 생성된 user ID로 세션 생성
    
    2. 유저가 존재하는 경우
    
    (1) 세션 존재 여부 확인
    
    기존 세션이 존재하는 경우 -> 그대로 사용
    기존 세션이 존재하지 않는 경우 -> 새 세션 생성
    
    1,2번 동작 후 client에게 세션 발급

--- auth-service/cmd/api/handle.go
+++ auth-service/cmd/api/handle.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	// Redis 패키지 import
+
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/baaami/dorandoran/auth/pkg/data"
 	"github.com/baaami/dorandoran/auth/pkg/types"
@@ -30,16 +33,67 @@ func (app *Config) KakaoLoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: 함수화
-	// 카카오 API 호출을 통해 access token 검증
+	// [Network] 카카오 API 호출을 통해 access token 검증
+	kakaoResponse, err := GetKaKaoUserInfoByAccessToken(requestData.AccessToken)
+	if err != nil {
+		http.Error(w, "Invalid Kakao token", http.StatusUnauthorized)
+	}
+
+	// 사용자 정보에서 카카오 사용자 ID 추출
+	kakaoUserID := fmt.Sprintf("%v", kakaoResponse["id"])
+
+	// [Hub Network] User 서비스에 API를 호출하여 존재하는 회원인지 확인
+	existUser, err := GetExistUserByUserSrv(types.KAKAO, kakaoUserID)
+	if err != nil {
+		fmt.Printf("Error occurred while checking user existence: %v\n", err)
+		return
+	}
+
+	var sessionID string
+
+	if (existUser == data.User{}) {
+		// 유저가 존재하지 않는 경우 -> 회원가입 진행
+		newUserID, err := RegisterNewUser(app, kakaoUserID)
+		if err != nil {
+			http.Error(w, "Failed to register new user", http.StatusInternalServerError)
+			return
+		}
+
+		// 생성된 user ID로 세션 생성
+		sessionID = app.RedisClient.CreateSession(strconv.Itoa(newUserID))
+
+	} else {
+		// 유저가 존재하는 경우 -> 세션 존재 여부 확인
+		sessionID, err = app.RedisClient.GetSessionByUserID(strconv.Itoa(existUser.ID))
+		if err == nil && sessionID != "" {
+			// 기존 세션이 존재하는 경우 -> 그대로 사용
+		} else {
+			// 기존 세션이 존재하지 않는 경우 -> 새 세션 생성
+			sessionID = app.RedisClient.CreateSession(strconv.Itoa(existUser.ID))
+			if err != nil {
+				http.Error(w, "Failed to create session", http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+
+	SetSessionCookie(&w, sessionID)
+
+	// 클라이언트에게 로그인 성공 응답
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Login successful, session ID issued"))
+}
+
+// [Network] 카카오 API 호출을 통해 access token 검증
+func GetKaKaoUserInfoByAccessToken(accessToken string) (map[string]interface{}, error) {
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", KAKAO_API_USER_INFO_URL, nil)
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", requestData.AccessToken))
+	req.Header.Set("Authorization", fmt.Sprintf("BearerO %s", accessToken))
 
 	resp, err := client.Do(req)
 	if err != nil || resp.StatusCode != 200 {
-		http.Error(w, "Invalid Kakao token", http.StatusUnauthorized)
-		return
+		log.Printf("Invalid Kakao token, statuscode: %d, err: %s", resp.StatusCode, err.Error())
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -48,60 +102,91 @@ func (app *Config) KakaoLoginHandler(w http.ResponseWriter, r *http.Request) {
 	var kakaoResponse map[string]interface{}
 	json.Unmarshal(body, &kakaoResponse)
 
-	// 사용자 정보에서 카카오 사용자 ID 추출
-	kakaoUserID := fmt.Sprintf("%v", kakaoResponse["id"])
-	snsID := app.RedisClient.MakeSnsID(types.KAKAO, kakaoUserID)
+	return kakaoResponse, nil
+}
 
-	// TODO: user-service로부터 존재하는 User 인지 확인
-	// 존재할 경우 존재하는 user id를 가져와서 바로 CreateSession 수행
-
-	// 기존 세션이 존재하는지 확인
-	sessionID, err := app.RedisClient.GetSessionBySnsID(snsID)
-	if err != nil {
-
-		// TODO: user-service로에 회원 가입 요청
-		client = &http.Client{}
-
-		newUser := data.User{
-			SnsType:  types.KAKAO,
-			SnsID:    kakaoUserID,
-			Name:     "", // Name은 빈 값으로 설정
-			Nickname: "", // Nickname은 빈 값으로 설정
-			Gender:   0,  // Gender는 기본값(0)으로 설정
-			Age:      0,  // Age는 기본값(0)으로 설정
-			Email:    "", // Email은 빈 값으로 설정
-		}
-
-		// JSON으로 변환
-		jsonData, err := json.Marshal(newUser)
-		if err != nil {
-			http.Error(w, "Failed to prepare user data", http.StatusInternalServerError)
-			return
-		}
-
-		req, _ = http.NewRequest("POST", "http://user-service/create", bytes.NewBuffer(jsonData))
-		req.Header.Set("Content-Type", "application/json")
-
-		resp, err = client.Do(req)
-		if err != nil || resp.StatusCode != 200 {
-			http.Error(w, "Invalid Kakao token", http.StatusUnauthorized)
-			return
-		}
-
-		// 응답에서 유저 ID 파싱
-		var createdUser data.User
-		err = json.NewDecoder(resp.Body).Decode(&createdUser)
-		if err != nil {
-			http.Error(w, "Failed to parse user-service response", http.StatusInternalServerError)
-			return
-		}
-
-		newUser.ID = createdUser.ID
-
-		// 세션이 존재하지 않으면 새로 생성
-		sessionID = app.RedisClient.CreateSession(strconv.Itoa(newUser.ID))
+// [Hub Network] User 서비스에 API를 호출하여 존재하는 회원인지 확인
+func GetExistUserByUserSrv(snsType int, snsID string) (data.User, error) {
+	client := &http.Client{
+		Timeout: time.Second * 10, // 요청 타임아웃 설정
 	}
 
+	// 요청 URL 생성
+	url := fmt.Sprintf("http://user-service/exist?sns_type=%d&sns_id=%s", snsType, snsID)
+
+	// GET 요청 생성
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return data.User{}, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	// 요청 실행
+	resp, err := client.Do(req)
+	if err != nil {
+		return data.User{}, fmt.Errorf("failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// 응답 처리
+	if resp.StatusCode == http.StatusNotFound {
+		// 유저가 존재하지 않는 경우
+		return data.User{}, nil
+	} else if resp.StatusCode != http.StatusOK {
+		// 다른 에러가 발생한 경우
+		return data.User{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// 응답 본문에서 유저 정보 디코딩
+	var user data.User
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		return data.User{}, fmt.Errorf("failed to decode response: %v", err)
+	}
+
+	// 유저가 존재하는 경우
+	return user, nil
+}
+
+// [Hub Network] User 서비스에 API를 호출하여 새로운 사용자 생성
+func RegisterNewUser(app *Config, kakaoUserID string) (int, error) {
+	newUser := data.User{
+		SnsType: types.KAKAO, // Kakao SNS 유형
+		SnsID:   kakaoUserID, // Kakao 사용자 ID
+	}
+
+	// user-service로 POST 요청 보내기
+	client := &http.Client{}
+	reqBody, err := json.Marshal(newUser)
+	if err != nil {
+		return 0, fmt.Errorf("failed to marshal new user data: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", "http://user-service/register", bytes.NewBuffer(reqBody))
+	if err != nil {
+		return 0, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, fmt.Errorf("failed to send request to user-service: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return 0, fmt.Errorf("failed to create user, status code: %d", resp.StatusCode)
+	}
+
+	var createdUser data.User
+	err = json.NewDecoder(resp.Body).Decode(&createdUser)
+	if err != nil {
+		return 0, fmt.Errorf("failed to decode response: %v", err)
+	}
+
+	return createdUser.ID, nil
+}
+
+func SetSessionCookie(w *http.ResponseWriter, sessionID string) {
 	cookie := &http.Cookie{
 		Name:     "session_id",
 		Value:    sessionID,
@@ -109,9 +194,5 @@ func (app *Config) KakaoLoginHandler(w http.ResponseWriter, r *http.Request) {
 		Secure:   true,
 		Path:     "/",
 	}
-	http.SetCookie(w, cookie)
-
-	// 클라이언트에게 로그인 성공 응답
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Login successful, session ID issued"))
+	http.SetCookie(*w, cookie)
 }
--- auth-service/pkg/redis/redis.go
+++ auth-service/pkg/redis/redis.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"time"
 
-	"github.com/baaami/dorandoran/auth/pkg/types"
 	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
 )
@@ -45,8 +43,8 @@ func NewRedisClient() (*RedisClient, error) {
 }
 
 // SetSession: Redis에 세션 저장
-func (r *RedisClient) SetSession(sessionID string, snsID string, expiresAt time.Duration) error {
-	err := r.Client.Set(ctx, sessionID, snsID, expiresAt).Err()
+func (r *RedisClient) SetSession(sessionID string, userID string, expiresAt time.Duration) error {
+	err := r.Client.Set(ctx, sessionID, userID, expiresAt).Err()
 	if err != nil {
 		log.Printf("Failed to set session in Redis: %v", err)
 		return err
@@ -56,13 +54,13 @@ func (r *RedisClient) SetSession(sessionID string, snsID string, expiresAt time.
 
 // GetSession: Redis에서 세션 조회
 func (r *RedisClient) GetSession(sessionID string) (string, error) {
-	snsID, err := r.Client.Get(ctx, sessionID).Result()
+	userID, err := r.Client.Get(ctx, sessionID).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("session not found")
 	} else if err != nil {
 		return "", err
 	}
-	return snsID, nil
+	return userID, nil
 }
 
 // DeleteSession: Redis에서 세션 삭제
@@ -75,9 +73,9 @@ func (r *RedisClient) DeleteSession(sessionID string) error {
 	return nil
 }
 
-// GetSessionBySnsID: 사용자 ID로 세션 ID 조회
-func (r *RedisClient) GetSessionBySnsID(snsID string) (string, error) {
-	sessionID, err := r.Client.Get(ctx, snsID).Result()
+// GetSessionByUserID: 사용자 ID로 세션 ID 조회
+func (r *RedisClient) GetSessionByUserID(userID string) (string, error) {
+	sessionID, err := r.Client.Get(ctx, userID).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("no session found for user")
 	} else if err != nil {
@@ -86,16 +84,8 @@ func (r *RedisClient) GetSessionBySnsID(snsID string) (string, error) {
 	return sessionID, nil
 }
 
-// GetSessionBySnsID: 사용자 ID로 세션 ID 조회
-func (r *RedisClient) MakeSnsID(snsType int, snsUserID string) string {
-	snsTypeStr := strconv.Itoa(snsType)
-	snsID := snsTypeStr + types.USER_ID_DELIMETER + snsUserID
-
-	return snsID
-}
-
 // 세션 생성 함수 (Redis에 세션 저장)
-func (r *RedisClient) CreateSession(snsID string) string {
+func (r *RedisClient) CreateSession(userID string) string {
 	// 고유한 세션 ID 생성 (UUID 사용)
 	sessionID := uuid.New().String()
 
@@ -103,7 +93,7 @@ func (r *RedisClient) CreateSession(snsID string) string {
 	expiresAt := time.Hour * 24
 
 	// Redis에 세션 저장
-	err := r.SetSession(sessionID, snsID, expiresAt)
+	err := r.SetSession(sessionID, userID, expiresAt)
 	if err != nil {
 		fmt.Printf("Failed to store session in Redis: %v", err)
 	}
