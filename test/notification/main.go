f5a84d0839171421a4fe990cc7986b6ae4f31382
baaami <lgh121546@naver.com>
2025-01-04 12:42:22 +0900

    onesignal 연동 테스트 코드 작성
    [변경된 모듈]
    test/notification
    
    [세부 사항]
    n/a

--- test/notification/main.go
+++ test/notification/main.go
@@ -9,12 +9,10 @@ import (
 
 func CreateNotifications(appID string, client *onesignal.Client) string {
 	fmt.Println("### CreateNotifications ###")
-	playerID := "69a7a386-6f00-42e0-9791-d0dcbc7d3ccf" // valid
-	// playerID := "83823c5f-53ce-4e35-be6a-a3f27e5d838f" // invalid
+	playerID := "c751f26e-22a0-4b87-9a21-96291678096b" // valid
 	notificationReq := &onesignal.NotificationRequest{
 		AppID:            appID,
-		Contents:         map[string]string{"en": "English message"},
-		IsIOS:            true,
+		Contents:         map[string]string{"en": "Test message"},
 		IncludePlayerIDs: []string{playerID},
 	}
 
@@ -30,12 +28,10 @@ func CreateNotifications(appID string, client *onesignal.Client) string {
 }
 
 func main() {
-	appID := "i am solo"
+	appID := "69a7a386-6f00-42e0-9791-d0dcbc7d3ccf"
 	appKey := "os_v2_app_ngt2hbtpabbobf4r2doly7j4z72ntargfmqe4r4y6h6xymwbfkgrrd6kqf24x4432olcysbylex4xmi7ec7ujfohaaj5zrivu4p4biy"
-	userKey := "os_v2_app_ngt2hbtpabbobf4r2doly7j4z72ntargfmqe4r4y6h6xymwbfkgrrd6kqf24x4432olcysbylex4xmi7ec7ujfohaaj5zrivu4p4biy"
 	client := onesignal.NewClient(nil)
 	client.AppKey = appKey
-	client.UserKey = userKey
 
 	notifID := CreateNotifications(appID, client)
 
